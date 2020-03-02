9133aa38a993c5d14f52636a218d5259f9d71b1f
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-02 14:11:39 +0200

    Phonebook step 7 done

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import Form from './Form';
 import Persons from './Persons';
 import Filter from './Filter';
+import phonebook from '../services/phonebook';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,10 +11,22 @@ const App = () => {
   const [newTerm, setNewTerm] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data));
+    phonebook.getAll().then(data => setPersons(data));
   }, []);
+
+  const addPerson = e => {
+    e.preventDefault();
+
+    if (checkPersonExists(newName)) {
+      alert(`${newName} is already added to the phonebook`);
+    } else {
+      const newPerson = { name: newName, number: newNumber };
+      phonebook.create(newPerson).then(p => setPersons(persons.concat(p)));
+    }
+
+    setNewName('');
+    setNewNumber('');
+  };
 
   const checkPersonExists = input => {
     return persons.some(person => person.name === input);
@@ -24,19 +36,6 @@ const App = () => {
     return persons.filter(person =>
       person.name.toLowerCase().includes(newTerm.toLowerCase())
     );
-  };
-
-  const onFormSubmit = e => {
-    e.preventDefault();
-
-    if (checkPersonExists(newName)) {
-      alert(`${newName} is already added to the phonebook`);
-    } else {
-      const newPerson = { name: newName, number: newNumber };
-      setPersons(persons.concat(newPerson));
-    }
-    setNewName('');
-    setNewNumber('');
   };
 
   const onNameChange = e => setNewName(e.target.value);
@@ -49,7 +48,7 @@ const App = () => {
       <Filter onTermChange={onTermChange} term={newTerm} />
       <h3>Add New</h3>
       <Form
-        onFormSubmit={onFormSubmit}
+        onFormSubmit={addPerson}
         onNameChange={onNameChange}
         onNumberChange={onNumberChange}
         name={newName}
