2c6101aee2bd854f94795220d94f28e5f018be1a
Emil Dimitrov <edimitrov1@gmail.com>
2020-04-26 15:01:50 +0300

    Helper functions and unit tests step 2 done

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, current) => {
+    return total + current.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
