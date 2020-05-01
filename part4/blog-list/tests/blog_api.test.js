ad28a01dbf9e87972bef83bdfa7c92b04eb952f5
Emil Dimitrov <edimitrov1@gmail.com>
2020-05-01 23:40:58 +0300

    Blog list tests step 4 done

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -56,6 +56,22 @@ test('new blog post can be created', async () => {
   expect(titles).toContain('Goodbye, Clean Code');
 });
 
+test('likes property has a default value of 0', async () => {
+  const newBlog = {
+    title: 'Goodbye, Clean Code',
+    author: 'Dan Abramov',
+    url: 'https://overreacted.io/goodbye-clean-code/',
+  };
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  expect(response.body.likes).toBe(0);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
