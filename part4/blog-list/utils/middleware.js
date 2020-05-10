51ef0b8cfb7eb5b9aadd82132658a4512e61035e
Emil Dimitrov <edimitrov1@gmail.com>
2020-05-10 14:58:40 +0300

    Blog list expansion step 8 done

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -6,6 +6,17 @@ const errorHandler = (err, request, response, next) => {
   next(err);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const auth = request.get('authorization');
+
+  if (auth && auth.toLowerCase().startsWith('bearer ')) {
+    request.token = auth.substring(7);
+  }
+
+  next();
+};
+
 module.exports = {
   errorHandler,
+  tokenExtractor,
 };
