ad6fdac85c0ec5c744380c62cd3ea05f1241413b
Emil Dimitrov <edimitrov1@gmail.com>
2020-07-31 17:58:20 +0300

    Anecdotes and the backend step 3

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -4,7 +4,7 @@ import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdotesList';
 import Notification from './components/Notification';
 import Filter from './components/Filter';
-import anecdoteService from './services/anecdotes';
+
 import { initializeAnecdotes } from './reducers/anecdoteReducer';
 
 const App = () => {
@@ -12,9 +12,7 @@ const App = () => {
   const notification = useSelector((state) => state.notification);
 
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+    dispatch(initializeAnecdotes());
   }, [dispatch]);
 
   return (
