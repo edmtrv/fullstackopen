4d0ff9f42002eefb156730743cd3e8802a1882b0
Emil Dimitrov <edimitrov1@gmail.com>
2020-08-07 19:34:14 +0300

    Anecdotes and hooks step 3 done

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -91,9 +91,9 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const content = useField('text');
-  const author = useField('text');
-  const info = useField('text');
+  const { reset: resetC, ...content } = useField('text');
+  const { reset: resetA, ...author } = useField('text');
+  const { reset: resetI, ...info } = useField('text');
 
   const history = useHistory();
 
@@ -110,9 +110,9 @@ const CreateNew = (props) => {
   };
 
   const reset = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    resetC();
+    resetA();
+    resetI();
   };
 
   return (
@@ -121,23 +121,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           Content
-          <input
-            name="content"
-            value={content.value}
-            onChange={content.onChange}
-          />
+          <input {...content} />
         </div>
         <div>
           Author
-          <input
-            name="author"
-            value={author.value}
-            onChange={author.onChange}
-          />
+          <input {...author} />
         </div>
         <div>
           URL for more info
-          <input name="info" value={info.value} onChange={info.onChange} />
+          <input {...info} />
         </div>
         <button>Create</button>
         <button type="button" onClick={reset}>
