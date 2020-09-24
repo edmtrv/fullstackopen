eac093a666c09ce552a85b51f1bf4184a01f0541
Emil Dimitrov <edimitrov1@gmail.com>
2020-09-24 15:06:30 +0300

    Database part 1 done

--- part8/library/src/index.js
+++ part8/library/src/index.js
@@ -1,5 +1,20 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { v1: uuid } = require('uuid');
+const mongoose = require('mongoose');
+
+const MONGODB_URI =
+  'mongodb+srv://emil:asddsa@cluster0.t347y.mongodb.net/libraryql?retryWrites=true&w=majority';
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connnection to MongoDB', error.message);
+  });
 
 let authors = [
   {
@@ -82,7 +97,7 @@ let books = [
 const typeDefs = gql`
   type Book {
     title: String!
-    author: String!
+    author: Author!
     published: Int!
     genres: [String!]!
     id: ID!
