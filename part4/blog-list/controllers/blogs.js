44ec96bc77056aa7e7e4767e6d52268d21fe7773
Emil Dimitrov <edimitrov1@gmail.com>
2020-10-07 22:59:12 +0300

    Update blog list backend

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -2,12 +2,15 @@ const jwt = require('jsonwebtoken');
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const Comment = require('../models/comment');
 
 blogsRouter.get('/', async (request, response) => {
-  const allBlogs = await Blog.find({}).populate('user', {
-    username: 1,
-    name: 1,
-  });
+  const allBlogs = await Blog.find({})
+    .populate('user', {
+      username: 1,
+      name: 1,
+    })
+    .populate('comments', { content: 1 });
 
   response.json(allBlogs);
 });
@@ -68,7 +71,6 @@ blogsRouter.delete('/:id', async (request, response) => {
 
 blogsRouter.put('/:id', async (request, response) => {
   const blog = {
-    author: request.body.author,
     title: request.body.title,
     url: request.body.url,
     likes: request.body.likes,
@@ -76,9 +78,36 @@ blogsRouter.put('/:id', async (request, response) => {
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
     new: true,
-  });
+  })
+    .populate('user', {
+      username: 1,
+      name: 1,
+    })
+    .populate('comments', { content: 1 });
 
   response.json(updatedBlog);
+});
+
+blogsRouter.get('/:id/comments', async (request, response) => {
+  const comments = await Comment.find({ blog: request.params.id });
+
+  response.json(comments);
+});
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const body = request.body;
+
+  const blog = await Blog.findById(request.params.id);
+  const comment = new Comment({
+    content: body.content,
+    blog: request.params.id,
+  });
+  const savedComment = await comment.save();
+
+  blog.comments = blog.comments.concat(savedComment);
+  await blog.save();
+
+  response.status(201).json(savedComment);
 });
 
 module.exports = blogsRouter;
