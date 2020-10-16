1ebe4a0c22fdf8a1d139f99fb9fb2439b2b6c9af
Emil Dimitrov <edimitrov1@gmail.com>
2020-10-17 00:49:54 +0300

    Patentior backend step 3 done

--- part9/patientor/src/index.ts
+++ part9/patientor/src/index.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.use('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
-const PORT = 3001;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
