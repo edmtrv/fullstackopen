d3226879f7136891a9b38aeb5eebf371d12a7709
Emil Dimitrov <edimitrov1@gmail.com>
2020-10-28 20:59:09 +0200

    Patientor step 2 done

---
+++ part9/patientor-frontend/src/PatientListPage/SinglePatient.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Container, Icon } from "semantic-ui-react";
+
+import { Patient } from "../types";
+import { apiBaseUrl } from "../constants";
+import { useStateValue } from "../state";
+
+const SinglePatient: React.FC = () => {
+  const [{ patient }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: singlePatient } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+
+        dispatch({ type: "SET_SINGLE_PATIENT", payload: singlePatient });
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPatient();
+  }, [dispatch]);
+
+  if (!patient) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Container>
+        <h2>
+          {patient.name}{" "}
+          <Icon className={patient.gender === "male" ? "mars" : "venus"}></Icon>
+        </h2>
+        <p>
+          ssn: {patient.ssn}
+          <br />
+          occupation: {patient.occupation}
+        </p>
+      </Container>
+    </div>
+  );
+};
+
+export default SinglePatient;
