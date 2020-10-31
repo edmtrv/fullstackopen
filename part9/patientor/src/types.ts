7d1138a870fa729b886280ab7162aaa5a400512f
Emil Dimitrov <edimitrov1@gmail.com>
2020-11-01 00:04:02 +0200

    Patentior step 4 done

--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -1,7 +1,43 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
-export interface Entry {}
+export type Entry = HospitalEntry | OccupationalHealthEntry | HealthCheckEntry;
 
-export interface Diagnose {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
